52974d716fb5c3d4b0102cd741282104d34cb10e
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-02-20 21:09:21 +0700

    Update kirim-telegram.js

--- api/kirim-telegram.js
+++ api/kirim-telegram.js
@@ -1,5 +1,6 @@
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
+
     // Pastikan cuma nerima metode POST dari HTML kamu
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Hanya menerima method POST' });
